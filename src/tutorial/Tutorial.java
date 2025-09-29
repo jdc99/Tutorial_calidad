9d6f2668e42b39664f4a47bc3203aa2d6d301b22
ifram <ifram@jTUFd>
2025-09-29 16:38:51 +0200

    Segundo

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -5,6 +5,8 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated m2ethod stub
 		System.out.println("Hola CMEPPS!");
+		System.out.println("Este es del commit 2!");
+
 	}
 
 }
